67822ffd89e7fbfd05c24c558c8ed8fd8590fd78
浪漫不属意 <1391936847@qq.com>
2019-04-02 00:07:45 +0800

    feat: 数据库操作

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ var express = require('express')
 var routes = require('./routes/index');
 
 var expressLayouts = require('express-ejs-layouts');
-
+var bodyParser = require('body-parser');
 var app = express()
+
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
 /**
  * set Views layout
  */
+
 app.use(expressLayouts);
 app.set('views', path.join(__dirname, 'views'));
 app.set("view engine",'ejs');
@@ -22,6 +26,8 @@ app.get('/signup', routes.signup);
 app.get('/admin', require('./routes/admin/index').index);
 app.get('/admin/login', require('./routes/admin/login').login);
 app.get('/admin/register', require('./routes/admin/login').register);
+app.get('/admin/register', require('./routes/admin/login').register);
+app.use('/user', require('./routes/admin/user'))
 
 app.listen(8080, () => console.log('Example app listening on port 8080!'))
 app.use(express.static("static"));
--- routes/admin/user.js
+++ routes/admin/user.js
@@ -1,15 +1,41 @@
-exports.list = function(req, res){
-    res.render("admin/index", {title: '登入', layout: 'admin/layout' });
-}
+var User = require("../../models/user.js");
+var express = require('express')
+var router = express.Router()
 
-exports.new = function(req, res){
-    res.render("admin/index", {title: '登入', layout: 'admin/layout' });
-}
+router.get('/', function(req, res){
+    User.find(function(err, docs){
+        console.log(docs);
+        res.json(docs)
+    })
+    // res.render("admin/index", {title: '登入', layout: 'admin/layout' });
+});
 
-exports.delete = function(req, res){
-    res.render("admin/index", {title: '登入', layout: 'admin/layout' });
-}
+router.post('/new', function(req, res){
+    console.log(req.body)
+    // var user = new User({
+    //     username : 'Tracy McGrady',                 //用户账号
+    //     userpwd: 'abcd',                            //密码
+    //     userage: 37,                                //年龄
+    //     phone: 1377078464,
+    // });
+    // user.save(function (err, result) {
+    //     if (err) {
+    //         console.log("Error:" + err);
+    //     }
+    //     else {
+    //         res.redirect('/user');    
+    //     }
+    // });
+    res.send(req.body.name)
+    // res.render("admin/index", {title: '登入', layout: 'admin/layout' });
+})
 
-exports.get = function(req, res){
-    res.render("admin/index", {title: '登入', layout: 'admin/layout' });
-}
+router.get('delete', function(req, res){
+    // res.render("admin/index", {title: '登入', layout: 'admin/layout' });
+});
+
+router.get('get', function(req, res){
+    // res.render("admin/index", {title: '登入', layout: 'admin/layout' });
+});
+
+module.exports = router;
